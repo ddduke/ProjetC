7233009ecaea959165197ebd60929734d88d7bb0
dduke <kikilefou0001@gmail.com>
2020-12-02 12:49:18 +0100

    basics movement units
    Setting basic movement on grid for units
    work in progress to display the path of the unit

---
+++ Unity/Assets/Scripts/COMBAT SCRIPTS/DisplayPath.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DisplayPath : MonoBehaviour
+{
+    public Camera cam;
+    public LineRenderer pathLine;
+    public Vector3 target;
+    private UnityEngine.AI.NavMeshPath path;
+    private float elapsed = 0.0f;
+    void Start()
+    {
+        path = new UnityEngine.AI.NavMeshPath();
+        elapsed = 0.0f;
+    }
+
+
+    void Update()
+    {
+
+
+        // Update the way to the goal every second.
+        elapsed += Time.deltaTime;
+        if (elapsed > 1.0f)
+        {
+            
+            elapsed -= 1.0f;
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity) && hit.collider.tag == "Ground")
+            {
+                Ground g = hit.collider.GetComponent<Ground>();
+                target = g.transform.position;
+                target.y += 0.5f;
+            }
+            UnityEngine.AI.NavMesh.CalculatePath(transform.position, target, UnityEngine.AI.NavMesh.AllAreas, path);
+        }
+        for (int i = 0; i < path.corners.Length - 1; i++)
+            pathLine.SetPosition(i,path.corners[i]);
+    }
+}
---
+++ Unity/Assets/Scripts/COMBAT SCRIPTS/UnitMovingToPoint.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class UnitMovingToPoint : MonoBehaviour
+{
+
+    public Camera cam;
+
+    public NavMeshAgent agent;
+
+    Vector3 relativeGroundPosition;
+
+    void Start()
+    {
+        Ground currentGround = GetCurrentGround();
+        relativeGroundPosition = transform.position - currentGround.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit) && hit.collider.tag == "Ground")
+            {
+                Ground g = hit.collider.GetComponent<Ground>();
+                Vector3 position = g.transform.position;
+                position += relativeGroundPosition;
+                agent.SetDestination(position);
+            }
+        }
+        
+    }
+
+    public Ground GetCurrentGround()
+    {
+        Ground currentGround = GetTargetGround(gameObject);
+        return currentGround;
+
+    }
+
+    public Ground GetTargetGround(GameObject target)
+    {
+        LayerMask layer_mask = LayerMask.GetMask("Ground");
+        RaycastHit hit;
+        Ground ground = null;
+        if (Physics.Raycast(target.transform.position, -Vector3.up, out hit, 1, layer_mask))
+        {
+            ground = hit.collider.GetComponent<Ground>();
+        }
+
+        return ground;
+    }
+}
